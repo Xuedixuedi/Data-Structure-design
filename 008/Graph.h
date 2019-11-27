20793385dcd0017574ffea704d053edee374c0a8
Xuedixuedi <958190137@qq.com>
2019-11-27 22:15:56 +0800

    finish code 8

--- 008/Graph.h
+++ 008/Graph.h
@@ -2,24 +2,154 @@
 // Created by lxd on 2019/11/22.
 //
 
-
 //用邻接矩阵建立图
-//用克鲁斯卡尔法构造最小生成树
+//用Prim法构造最小生成树
 #ifndef INC_008_GRAPH_H
 #define INC_008_GRAPH_H
 
-
 class Graph {
-private:
-    char *_name;//储存顶点名称的数组
-    int **_matrix;//邻接矩阵表示
-public:
-    int _size;//顶点数
-    Graph() = default;
-    Graph(int size, char *name, int **matrix = nullptr) :
-            _size(size), _name(name), _matrix(matrix) {}
+   private:
+    char *_name;         //储存顶点名称的数组
+    int **_matrix;       //邻接矩阵表示
+    bool **_primMatrix;  //最小生成树的边
+    int _size;           //顶点数
+    int *_parent;        //最小生成树里面的父节点
+    bool *_visit;
+    int *_score;
+    int _sum;  //最短路径和
 
+   public:
+    Graph() = default;
+    // Graph(int size, char *name, int **matrix = nullptr)
+    //     : _size(size), _name(name), _matrix(matrix) {}
+    Graph(int size, char *name, int **matrix) {
+        _size = size;
+        _name = name;
+        _sum = 0;
+        _matrix = matrix;
+        _parent = new int[size];
+        _parent[size] = {-1};
+        _visit = new bool[size];
+        _visit[size] = {false};
+        _score = new int[size];
+        _score[size] = {INT_MAX};
+        _primMatrix = new bool *[size];
+        _primMatrix[size] = new bool[size];
+        _primMatrix[size][size] = {false};
+    }
+    void initMatrix(char vertex1, char vertex2, int side);
+    void printMatrix();
+    void prim(char vertex);  //以vertex为顶点生成最小生成树
+    void displayTree();
 };
 
+void Graph::initMatrix(char vertex1, char vertex2, int side) {
+    for (int i = 0; i < _size; ++i) {
+        if (_name[i] == vertex1 || _name[i] == vertex2) {
+            for (int j = i + 1; j < _size; ++j) {
+                if (_name[j] == vertex2 || _name[j] == vertex1) {
+                    _matrix[i][j] = side;
+                    _matrix[j][i] = side;
+                    break;
+                }
+            }
+        }
+    }
+}
 
-#endif //INC_008_GRAPH_H
+void Graph::printMatrix() {
+    for (int i = 0; i < _size; ++i) {
+        for (int j = 0; j < _size; ++j) {
+            std::cout << _matrix[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
+}
+
+// void Graph::prim(char vertex) {
+//     int s;
+//     int sum = 0;
+//     for (int i = 0; i < _size; ++i) {
+//         if (_name[i] == vertex) {
+//             s = i;
+//             break;
+//         }
+//     }
+//     _score[s] = 0;
+//     for (int i = 0; i < _size; ++i) {
+//         int u = -1;  // u使得_score[u]最小
+//         int min = INT_MAX;
+//         for (int j = 0; j < _size; ++j) {
+//             if (_visit[j] == false && _score[j] < min) {
+//                 min = _score[j];
+//                 u = j;
+//                 _parent[u] = i;
+//             }
+//         }
+//         if (u == -1) {
+//             exit(-1);
+//         }
+//         _visit[u] = true;
+//         sum += _score[u];
+//         for (int v = 0; v < _size; ++v) {
+//             if (_visit[v] == false && _matrix[u][v] < INT_MAX &&
+//                 _matrix[u][v] < _score[v]) {
+//                 _score[v] = _matrix[u][v];
+//                 _parent[v] = u;
+//             }
+//         }
+//     }
+//     for (int i = 0; i < _size; ++i) {
+//         std::cout << _parent[i] << ' ';
+//     }
+//     std::cout << sum;
+// }
+
+void Graph::prim(char vertex) {
+    //维护三个东西：selected minDist parent
+    int s;
+    //找到vertex对应的下标
+    for (int i = 0; i < _size; ++i) {
+        if (_name[i] == vertex) {
+            s = i;
+        }
+    }
+    for (int i = 0; i < _size; ++i) {
+        _score[i] = _matrix[s][i];
+        _parent[i] = s;
+        _visit[i] = false;
+    }
+    _visit[s] = true;
+
+    int minCost = 0;
+    int minid = 0;
+    for (int i = 0; i < _size - 1; ++i) {
+        minCost = INT_MAX;
+        for (int j = 0; j < _size; ++j) {
+            if (_visit[j] == false && _score[j] < minCost) {
+                minCost = _score[j];
+                minid = j;
+            }
+        }
+        _visit[minid] = true;
+        _sum += minCost;
+
+        for (int j = 0; j < _size; ++j) {
+            if (_visit[j] == false && _matrix[minid][j] < _score[j]) {
+                _score[j] = _matrix[minid][j];
+                _parent[j] = minid;
+            }
+        }
+    }
+    std::cout << "Generate Prim Minimum Spanning Tree!" << std::endl;
+}
+
+void Graph::displayTree() {
+    for (int i = 0; i < _size; ++i) {
+        if (_score[i] != INT_MAX) {
+            std::cout << _name[i] << "-(" << _score[i] << ")->"
+                      << _name[_parent[i]] << "\t\t";
+        }
+    }
+}
+#endif  // INC_008_GRAPH_H
--- 008/main.cpp
+++ 008/main.cpp
@@ -1,30 +1,48 @@
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 #include "Graph.h"
 
 using namespace std;
 
 void menu();
 
-Graph *init();//初始化图
-void addSlide(Graph *grid);
+Graph *init();  //初始化图
+void addSide(Graph *grid);
+void minTree(Graph *grid);
+void showTree(Graph *grid);
 
 int main() {
     menu();
-    cout << "Please select an operation: " << endl;
-    char op;
-    cin >> op;
     Graph *grid;
-    switch (op) {
-        case 'A': {
-            grid = init();
-            cout << endl;
+    while (1) {
+        cout << "Please select an operation: ";
+        char op;
+        cin >> op;
+        if (op == 'E') {
+            cout << "You are already exited the system" << endl;
             break;
         }
-        case 'B': {
-
-            cout << endl;
-            break;
+        switch (op) {
+            case 'A': {
+                grid = init();
+                cout << endl;
+                break;
+            }
+            case 'B': {
+                addSide(grid);
+                cout << endl;
+                break;
+            }
+            case 'C': {
+                minTree(grid);
+                cout << endl;
+                break;
+            }
+            case 'D': {
+                showTree(grid);
+                cout << endl;
+                break;
+            }
         }
     }
     return 0;
@@ -33,11 +51,21 @@ int main() {
 void menu() {
     cout << "**       Power grid cost simulation system        **" << endl;
     cout << "====================================================" << endl;
-    cout << std::left << setfill(' ') << setw(50) << "**     A --- Create a power grid vertex" << "**" << endl;
-    cout << std::left << setfill(' ') << setw(50) << "**     B --- Adding the side of the grid" << "**" << endl;
-    cout << std::left << setfill(' ') << setw(50) << "**     C --- Constructing minimum spanning tree" << "**" << endl;
-    cout << std::left << setfill(' ') << setw(50) << "**     D --- Display minimum spanning tree" << "**" << endl;
-    cout << std::left << setfill(' ') << setw(50) << "**     E --- Exit the program" << "**" << endl;
+    cout << std::left << setfill(' ') << setw(50)
+         << "**     A --- Create a power grid vertex"
+         << "**" << endl;
+    cout << std::left << setfill(' ') << setw(50)
+         << "**     B --- Adding the side of the grid"
+         << "**" << endl;
+    cout << std::left << setfill(' ') << setw(50)
+         << "**     C --- Constructing minimum spanning tree"
+         << "**" << endl;
+    cout << std::left << setfill(' ') << setw(50)
+         << "**     D --- Display minimum spanning tree"
+         << "**" << endl;
+    cout << std::left << setfill(' ') << setw(50)
+         << "**     E --- Exit the program"
+         << "**" << endl;
     cout << "====================================================" << endl;
 }
 
@@ -45,25 +73,51 @@ Graph *init() {
     cout << "Please enter the number of vertex: ";
     int size = 0;
     cin >> size;
+    while (size < 2) {
+        cout << "the number of vertex cannnot be less than 2. Please enter "
+                "again: ";
+        cin >> size;
+    }
     cout << "Please enter the names of the vertexes in order: ";
     char name;
     char *namelist = new char;
+    auto matrix = new int *[size];
     for (int i = 0; i < size; ++i) {
         cin >> name;
         namelist[i] = name;
+        matrix[i] = new int[size];
+        for (int j = 0; j < size; ++j) {
+            matrix[i][j] = INT_MAX;
+        }
     }
-    auto grid = new Graph(size, namelist);
+
+    auto grid = new Graph(size, namelist, matrix);
     return grid;
 }
 
-void addSlide(Graph *grid) {
+void addSide(Graph *grid) {
     char vertex1, vertex2;
-    int slide;
+    int side;
     while (true) {
-        cout << "请输入两个顶点及边： ";
-        cin >> vertex1 >> vertex2 >> slide;
+        cout << "Please enter two vertices and edges: ";
+        cin >> vertex1 >> vertex2 >> side;
         if (vertex1 == '?') {
             break;
+        } else {
+            grid->initMatrix(vertex1, vertex2, side);
         }
     }
+    // grid->printMatrix();
+}
+
+void minTree(Graph *grid) {
+    cout << "Please enter the starting vertex: ";
+    char v;
+    cin >> v;
+    grid->prim(v);
+}
+
+void showTree(Graph *grid) {
+    cout << "The vertices and edges of the minimum spanning tree are: " << endl;
+    grid->displayTree();
 }
